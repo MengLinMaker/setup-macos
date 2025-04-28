bc14de3c9743fcc73e2f275ad5d5db376a41fd59
Meng Lin <menglinmaker@gmail.com>
2025-04-28 16:47:23 +1000

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,35 @@
+# Download homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Install essential desktop apps
+brew install --cask visual-studio-code
+brew install --cask brave-browser
+brew install --cask warp
+brew install --cask docker
+
+# CLI tools
+brew install ripgrep-all
+brew install fzf
+brew install lazygit
+brew install lazydocker
+brew install curl
+
+# Cloud tools
+brew install aws-sam-cli
+
+# Kubernetes tools
+brew install kubernetes-cli
+brew install helm
+brew install tilt
+
+# JavaScript
+brew install node
+
+# Java
+brew install openjdk
+brew install gradle
+brew install maven
+
+# Csharp
+brew install dotnet
+brew install --cask dotnet-sdk
